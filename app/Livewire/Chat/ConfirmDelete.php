ea117d074b7de32ea79e88ea4c24fad23d06191e
Stefano Novelli <murdercode@gmail.com>
2023-08-08 14:59:30 +0200

    Confirm delete modal

---
+++ app/Livewire/Chat/ConfirmDelete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Chat;
+
+use Livewire\Component;
+
+class ConfirmDelete extends Component
+{
+    public function render()
+    {
+        return view('livewire.chat.confirm-delete');
+    }
+}
