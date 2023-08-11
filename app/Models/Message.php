874ed41004f47cd14ea5b7bbfd6416a619edc1fb
Stefano Novelli <murdercode@gmail.com>
2023-08-11 23:43:38 +0200

    wip

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -16,7 +16,7 @@ class Message extends Model
 
     public function getFromUserAttribute()
     {
-        return $this->in_out === true;
+        return $this->in_out == true;
     }
 
     public function getFromAiAttribute()
