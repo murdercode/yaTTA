7b2508d8630df8c5ec108d6d32a73e5d22a69a86
Stefano Novelli <murdercode@gmail.com>
2023-08-12 22:18:51 +0200

    Check if there's at least an API

--- resources/views/livewire/chat-ui.blade.php
+++ resources/views/livewire/chat-ui.blade.php
@@ -1,12 +1,20 @@
 <div class="flex-1 flex flex-col h-screen">
 
-    {{--Chat--}}
-    <livewire:chat.chat-messages :chat="$chat"/>
+    @if(config('openai.api_key'))
+        {{--Chat--}}
+        <livewire:chat.chat-messages :chat="$chat"/>
 
-    {{--Input--}}
-    <livewire:chat.chat-input :chat="$chat" input=""/>
+        {{--Input--}}
+        <livewire:chat.chat-input :chat="$chat" input=""/>
 
-    {{--Confirm Delete--}}
-    {{--    <livewire:chat.confirm-delete />--}}
+    @else
+
+        <x-chat.notification>
+            {{__('Please set up your OpenAI API key first')}}.
+            <a class="text-white underline" href="{{route('settings')}}">{{__('Go to Settings page')}}</a>
+        </x-chat.notification>
+
+    @endif
+
 
 </div>
