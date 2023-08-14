ce6e5e15069b7ba4017f0b4f082a726b3ccde9c2
Stefano Novelli <murdercode@gmail.com>
2023-08-14 16:18:24 +0200

    Make delete button works

--- app/Livewire/Chat/QuickSettings.php
+++ app/Livewire/Chat/QuickSettings.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire\Chat;
 
+use App\Models\Chat;
 use Livewire\Component;
 
 class QuickSettings extends Component
 {
+    public function deleteAllChats()
+    {
+        Chat::truncate();
+
+        return redirect()->route('welcome');
+    }
+
     public function render()
     {
         return view('livewire.chat.quick-settings');
